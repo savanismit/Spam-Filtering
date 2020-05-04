1ef054394329bc95426956c9552f08cf9b0bf55d
savanismit <savanismit11@example.com>
2020-05-04 13:59:24 +0530

    Changed Message

--- Spam_Prediction.py
+++ Spam_Prediction.py
@@ -12,6 +12,10 @@ def Prediction(mess):
     pickle_f = open('pipeline.pickle','rb')
     trans = pickle.load(pickle_f)
     pred = trans.predict(mess)
-    print(''.join(pred))
+    pred = ''.join(pred)
+    if pred == 'ham':
+        print("Normal Message")
+    else:
+        print("It's a Spam Message")
 
 Prediction(input())
